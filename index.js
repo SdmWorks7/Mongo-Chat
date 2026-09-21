613174ebe61a3098b64b293d45bf1be34f724726
Saumyadeep Mishra <sdm.cord7@gmail.com>
2026-09-21 23:20:38 +0530

    add: whole update endpoint created

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require ("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const Chat = require("./models/chat.js")
+const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 
 main()
@@ -22,7 +24,6 @@ async function main(){
 //index Route
 app.get("/chat",  async (req, res)=>{
     let chats= await Chat.find();
-    console.log(chats);
     res.render("index.ejs", {chats});
 });
 
@@ -30,7 +31,7 @@ app.get("/chats/new",  async (req, res)=>{
     res.render("new.ejs");
 });
 
-app.post("/chats", async(req, res)=>{
+app.post("/chats", async (req, res)=>{
     let { from, to, msg } = req.body;
     Chat.create({
         from: from,
@@ -38,6 +39,23 @@ app.post("/chats", async(req, res)=>{
         msg: msg,
         created_at: new Date(),
     });
+    res.redirect("http://localhost:8000/chat");
+});
+
+app.get("/chats/:id/edit",  async (req, res)=>{
+    let {id} = req.params;
+    let editable = await Chat.findById(id);
+    res.render("edit.ejs", {editable});
+});
+
+app.put("/chats/:id",  async (req, res)=>{
+    let {id } = req.params;
+    let {msg} = req.body;
+    let edited = await Chat.findByIdAndUpdate(id, {msg}, {
+        returnDocument: 'after',
+        runValidators: true,
+    });
+    console.log(edited);
     res.redirect("http://localhost:8000/chat");
 });
 
